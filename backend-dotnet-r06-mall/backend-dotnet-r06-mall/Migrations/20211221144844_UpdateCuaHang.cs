efb4582258909b09ec004925b9f47b4e388e6510
HaoNguyen <vanhao0643@gmail.com>
2021-12-21 22:05:20 +0700

    update store

---
+++ backend-dotnet-r06-mall/backend-dotnet-r06-mall/Migrations/20211221144844_UpdateCuaHang.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend_dotnet_r06_mall.Migrations
+{
+    public partial class UpdateCuaHang : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "KinhDo",
+                table: "CuaHang",
+                type: "decimal(18,10)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ViDo",
+                table: "CuaHang",
+                type: "decimal(18,10)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KinhDo",
+                table: "CuaHang");
+
+            migrationBuilder.DropColumn(
+                name: "ViDo",
+                table: "CuaHang");
+        }
+    }
+}
--- backend-dotnet-r06-mall/backend-dotnet-r06-mall/Models/CuaHang.cs
+++ backend-dotnet-r06-mall/backend-dotnet-r06-mall/Models/CuaHang.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend_dotnet_r06_mall.Models
 {
@@ -20,5 +21,10 @@ namespace backend_dotnet_r06_mall.Models
         public string SoDienThoai { get; set; }
         [MaxLength(30)]
         public string STK { get; set; }
+
+        [Column(TypeName = "decimal(18,10)")]
+        public decimal KinhDo { get; set; }
+        [Column(TypeName = "decimal(18,10)")]
+        public decimal ViDo { get; set; }
     }
 }
