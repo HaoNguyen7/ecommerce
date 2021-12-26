4036cbadad9167658c03d903934f3160ee0af389
hieutran25 <hieutranvan25@gmail.com>
2021-12-27 00:26:23 +0700

    + login ui

--- frontend-r06-mall/src/views/Login/login.js
+++ frontend-r06-mall/src/views/Login/login.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
-
-
-function Login() {
+import { Card, Input, Button } from "antd"
+import "./logIn.css"
+function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const getPassword = (event) => {
+    setPassword(event.target.value)
+  }
+  const getEmail = (event) => {
+    setEmail(event.target.value)
+  }
+  
+  const onClickSubmit = () => {
+    console.log(email)
+    console.log(password)
+  }
+  
   return (
-    <div>
-      Login
-    </div>
+    <Card title="Đăng nhập" className='login-card'>
+      <Input size="large" placeholder="large size" addonBefore="Email" type="email" onChange={getEmail} />
+      <Input size="large" placeholder="large size" addonBefore="Mật khẩu" type="password" onChange={getPassword} />
+      <Button className="login-button" type="primary" size="large" onClick={onClickSubmit}>
+          Đăng nhập
+      </Button>
+    </Card>
   );
 }
 
-export default Login;
+export default LogIn;
