ba975261d93eef4a88d02182f74d95eb0ae1d19d
bachtangthang <thang5217113@gmail.com>
2021-12-29 00:02:17 +0700

    commit bi loi

---
+++ backend-dotnet-r06-mall/backend-dotnet-r06-mall/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend_dotnet_r06_mall.Models;
+using backend_dotnet_r06_mall.Requests;
+using backend_dotnet_r06_mall.Response;
+using backend_dotnet_r06_mall.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace backend_dotnet_r06_mall.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly CartServices _service;
+        private readonly ILogger<CartController> _logger;
+
+        public CartController(CartServices service, ILogger<CartController> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct([FromQuery] ProductListRequest query)
+        {
+            PagedList<SanPham> listProducts = await _service.GetProducts(query);
+            return Ok(new PagedListResponse<SanPham>(listProducts));
+        }
+
+
+
+    }
+}
---
+++ backend-dotnet-r06-mall/backend-dotnet-r06-mall/Services/CartServices.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend_dotnet_r06_mall.Data;
+using backend_dotnet_r06_mall.Models;
+using backend_dotnet_r06_mall.Requests;
+using backend_dotnet_r06_mall.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_dotnet_r06_mall.Services
+{
+    public class CartServices
+    {
+        private readonly BanHangContext _context;
+
+
+        public CartServices(BanHangContext context)
+        {
+            _context = context;
+        }
+
+        public SanPham? GetById(int id)
+        {
+            return _context.SanPham!
+            .Include(p => p.TenSanPham)
+            .Include(p=> p.SanPhamId)
+            .Include(p=>p.DonGia)
+            .Include(p=>p.DonVi)
+            .Include(p=>p.CuaHang)
+            .AsNoTracking()
+            .SingleOrDefault(p=>p.SanPhamId = id);
+        }
+    }
+}
