22e28b27592fe8f88280d685810c476981ff2119
phmhngn <anphamimpala@gmail.com>
2021-12-29 12:54:04 +0700

    update

--- backend-dotnet-r06-mall/backend-dotnet-r06-mall/Services/OrderServices.cs
+++ backend-dotnet-r06-mall/backend-dotnet-r06-mall/Services/OrderServices.cs
@@ -6,17 +6,18 @@ using backend_dotnet_r06_mall.Models;
 using backend_dotnet_r06_mall.Requests;
 using backend_dotnet_r06_mall.Response;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace backend_dotnet_r06_mall.Services
 {
     public class OrderServices
     {
         private readonly BanHangContext _context;
-
-
-        public OrderServices(BanHangContext context)
+        private readonly ILogger<OrderServices> _logger;
+        public OrderServices(BanHangContext context, ILogger<OrderServices> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<PagedList<DonHang>> GetUserOrder(Guid userId, OrderHistoryRequest query)
@@ -27,7 +28,7 @@ namespace backend_dotnet_r06_mall.Services
 
         public async Task<DonHang> GetOrderById(Guid orderId)
         {
-            return await _context.DonHang.Include(o => o.KhachHang).FirstOrDefaultAsync(p => p.DonHangId == orderId);
+            return await _context.DonHang.Include(o => o.KhachHang).Include(o => o.DonHangSanPham).Include(o => o.SanPham).FirstOrDefaultAsync(p => p.DonHangId == orderId);
         }
 
     }
