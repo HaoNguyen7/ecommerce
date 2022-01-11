df2b155d28a28d29928acec6413e64b97fa6c97d
bachtangthang <thang5217113@gmail.com>
2022-01-11 17:22:25 +0700

    adding redux in cart function(include reducers, action, store and localstorage) (not include cartScreen cartButton or any useEffect hook)

---
+++ frontend-r06-mall/src/actions/cartActions.js
@@ -0,0 +1,24 @@
+import Axios from 'axios';
+import { CART_ADD_ITEM } from '../constants/cartConstants';
+
+export const addToCart = (productId, qty) => async (dispatch, getState) => {
+	const { data } = await Axios.get(`/api/products/${productId}`);
+	dispatch({
+		type: CART_ADD_ITEM,
+		payload: {
+			//contain product
+			name: data.name,
+			image: data.image,
+			price: data.price,
+			countInStock: data.countInStock,
+			product: data._id,
+			qty
+		}
+	});
+	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems)); //refesh trang ko mat data trong gi hang-redux
+};
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+	dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems)); //refesh trang ko mat data trong gi hang-redux
+};
--- frontend-r06-mall/src/views/Login/login.js
+++ frontend-r06-mall/src/views/Login/login.js
@@ -1,48 +1,57 @@
 import React, { useState } from 'react';
-import { Card, Input, Button } from "antd";
-import { Link } from "react-router-dom";
-import "./logIn.css";
-import axios from "axios"
-import {useNavigate } from "react-router-dom"
+import { Card, Input, Button } from 'antd';
+import { Link } from 'react-router-dom';
+import './logIn.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 function LogIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("")
-  const navigate = useNavigate();
-  const getPassword = (event) => {
-    setPassword(event.target.value)
-  }
-  const getEmail = (event) => {
-    setEmail(event.target.value)
-  }
-  
-  const onClickSubmit = () => {
-    axios({
-      method:"POST",
-      url: "https://localhost:5001/Accounts/Login",
-      data: {
-        email: email,
-        password: password 
-      }
-    }).then(res=> {
-      localStorage.setItem("token", res.data.token)
-      alert("Đăng nhập thành công")
-      navigate("/")
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const navigate = useNavigate();
+	const getPassword = (event) => {
+		setPassword(event.target.value);
+	};
+	const getEmail = (event) => {
+		setEmail(event.target.value);
+	};
 
-    }).catch(err=> {
-      console.log(err)
-    })
-  }
-  
-  return (
-    <Card title="Đăng nhập" className='login-card'>
-      <Input size="large" placeholder="Enter email" addonBefore="Email" type="email" onChange={getEmail} />
-      <Input size="large" placeholder="Enter password" addonBefore="Mật khẩu" type="password" onChange={getPassword} />
-      <Button type="primary" size="large" onClick={onClickSubmit}>
-          Đăng nhập
-      </Button>
-      <p><Link to="/signup">Tạo tài khoản</Link> nếu bạn chưa có tài khoản</p>
-    </Card>
-  );
+	const onClickSubmit = () => {
+		axios({
+			method: 'POST',
+			url: 'https://localhost:5001/Accounts/Login',
+			data: {
+				email: email,
+				password: password
+			}
+		})
+			.then((res) => {
+				localStorage.setItem('token', res.data.token);
+				alert('Đăng nhập thành công');
+				navigate('/');
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<Card title="Đăng nhập" className="login-card">
+			<Input size="large" placeholder="Enter email" addonBefore="Email" type="email" onChange={getEmail} />
+			<Input
+				size="large"
+				placeholder="Enter password"
+				addonBefore="Mật khẩu"
+				type="password"
+				onChange={getPassword}
+			/>
+			<Button type="primary" size="large" onClick={onClickSubmit}>
+				Đăng nhập
+			</Button>
+			<p>
+				<Link to="/signup">Tạo tài khoản</Link> nếu bạn chưa có tài khoản
+			</p>
+		</Card>
+	);
 }
 
 export default LogIn;
