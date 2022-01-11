b75bbdf2882a43b3100f5257389342b3b02bb252
bachtangthang <thang5217113@gmail.com>
2022-01-11 11:53:17 +0700

    thanh toan & shipping not done

--- backend-dotnet-r06-mall/backend-dotnet-r06-mall/Controllers/CartController.cs
+++ backend-dotnet-r06-mall/backend-dotnet-r06-mall/Controllers/CartController.cs
@@ -43,5 +43,23 @@ namespace backend_dotnet_r06_mall.Controllers
             var dh = await _service.TaoDonHang(gh, userId);
             return dh ? Ok() : BadRequest();
         }
+
+        [HttpPatch("Pay")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = RoleConstants.Khach)]
+        public async Task<IActionResult> ThanhToan([FromBody] Guid cartId)
+        {
+            Guid userId = new Guid(User.FindFirst("Id")?.Value);
+            var temp = await _service.thanhToan(cartId,userId);
+            return temp ? Ok(): BadRequest();
+        }
+
+        [HttpPost("Ship")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = RoleConstants.Khach)]
+        public async Task<IActionResult> Shipping([FromBody] CartRequest gh)
+        {
+            Guid userId = new Guid(User.FindFirst("Id")?.Value);
+            var dh = await _service.TaoDonHang(gh, userId);
+            return dh ? Ok() : BadRequest();
+        }
     }
 }
