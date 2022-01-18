63d8d29e90442b1bcd041683e2099a4c36cef12e
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2022-01-18 19:07:44 +0700

    Update Delivered.jsx

--- frontend-r06-mall/src/views/Driver/Delivered/Delivered.jsx
+++ frontend-r06-mall/src/views/Driver/Delivered/Delivered.jsx
@@ -3,17 +3,38 @@ import axios from "axios";
 import {Button,Card} from "antd"
 
 const Delivered = () => {
+    const [deliveredItems, setDeliveredItems] = useState([])
+    const [pageIndex, setPageIndex] = useState(0)
+    const [totalPage, setTotalPage] = useState(0)
 
-    const showDelivered = () => {
-        
+    const getDeliveredItems = () => {
+        axios.get("https://localhost:5001/api/Delivered/history")
+        .then(res=>{
+            const result = res.data
+            setDeliveredItems(result.data)
+            setPageIndex(result.pageIndex)
+            setTotalPage(result.totalPage)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
     }
+
+    useEffect(()=>{
+        getDeliveredItems()
+    }, [deliveredItems])
 
     return(
         <div>
-            <Card 
-                title="Đơn hàng đã giao" 
-                extra={<Button onClick={showDelivered}>Đơn hàng đã giao</Button>}>
-
+            <Card title="Đơn hàng đã giao">
+                <div className='delivered-list'>
+                    {deliveredItems.map((item)=>{
+                        const {} = item
+                        return(
+                            <h3>Delivered Item</h3>
+                        )
+                    })}
+                </div>
             </Card>
         </div>
     )
