d6832cf7bfd254b1ad66d84b2be85f9229af2a62
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2022-01-22 14:43:19 +0700

    update delivered ui

--- frontend-r06-mall/src/views/Driver/Delivered/Delivered.jsx
+++ frontend-r06-mall/src/views/Driver/Delivered/Delivered.jsx
@@ -3,10 +3,38 @@ import axios from "axios";
 import {Button,Card} from "antd"
 import "./Delivered.css"
 
+// const data = [
+//     {
+//         DonHangId: 111111,
+//         TinhTrangThanhToan: 'Da thanh toan',
+//         TinhTrangGiao: 'Dang giao hang',
+//         KhachHang: {
+//             TenKhachHang: 'Nguyễn Văn A',
+//             DiaChi: 'số nhà 1'
+//         }
+//     },
+//     {
+//         DonHangId: 222222,
+//         TinhTrangThanhToan: 'Da thanh toan',
+//         TinhTrangGiao: 'Da giao hang',
+//         KhachHang: {
+//             TenKhachHang: 'Nguyễn Văn B',
+//             DiaChi: 'số nhà 2'
+//         }
+//     },
+//     {
+//         DonHangId: 333333,
+//         TinhTrangThanhToan: 'Chua thanh toan',
+//         TinhTrangGiao: 'Dang giao hang',
+//         KhachHang: {
+//             TenKhachHang: 'Nguyễn Văn C',
+//             DiaChi: 'số nhà 3'
+//         }
+//     }
+// ]
+
 const Delivered = () => {
     const [deliveredItems, setDeliveredItems] = useState([])
-    const [pageIndex, setPageIndex] = useState(1)
-    const [pageSize, setPageSize] = useState(10)
 
     const getDeliveredItems = () => {
         axios({
@@ -17,8 +45,6 @@ const Delivered = () => {
         .then(res=>{
             const result = res.data
             setDeliveredItems(result.data)
-            setPageIndex(result.pageIndex)
-            setPageSize(result.pageSize)
         })
         .catch((error)=>{
             console.log(error)
@@ -36,19 +62,30 @@ const Delivered = () => {
     return(
         <div>
             <Card title="Đơn hàng đã giao">
+                <div className='label'>
+                    <h3>Mã đơn hàng</h3>
+                    <h3>Tình trạng thanh toán</h3>
+                    <h3>Tình trạng giao hàng</h3>
+                    <h3>Tên khách hàng</h3>
+                    <h3>Địa chỉ giao hàng</h3>
+                </div>
                 <div className='delivered-list'>
                     {deliveredItems.map((item)=>{
-                        const {} = item
+                        const {DonHangId, TinhTrangThanhToan, TinhTrangGiao, KhachHang} = item
                         return(
-                            <div className='delivered-item'>
-                                <h3>Đơn hàng đã vận chuyển</h3>
+                            <div className='delivered-items'>
+                                <h3> {DonHangId}</h3>
+                                <h3> {TinhTrangThanhToan}</h3>
+                                <h3> {TinhTrangGiao}</h3>
+                                <h3> {KhachHang.TenKhachHang}</h3>
+                                <h3> {KhachHang.DiaChi}</h3>
                             </div>
                         )
                     })}
                 </div>
                 <a 
                     className='show-more-btn'
-                    onClick={showMore()}
+                    onClick={showMore}
                 >
                     Xem thêm...
                 </a>
