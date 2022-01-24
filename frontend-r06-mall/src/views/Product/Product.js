03c5806248b037e3a3b5cf003dc3bc021a53f276
phmhngn <anphamimpala@gmail.com>
2022-01-24 19:28:11 +0700

    fix loaisp

--- frontend-r06-mall/src/views/Product/Product.js
+++ frontend-r06-mall/src/views/Product/Product.js
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Image, InputNumber, Button, Descriptions, Layout } from 'antd';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-
-const { Content } = Layout;
+import './Product.css';
+import 'antd/dist/antd.css';
 
 export default function Product() {
   const id = useParams().id;
   const [product, setProduct] = useState({});
-  let loaiSanPham;
 
   useEffect(() => {
     const getProduct = async () => {
@@ -21,7 +20,8 @@ export default function Product() {
     console.log(product.loaiSanPham);
     // eslint-disable-next-line
   }, []);
-  loaiSanPham = product.loaiSanPham;
+  const { loaiSanPham } = product;
+  const { cuaHang } = product;
 
   function onChange(value) {
     console.log('changed', value);
@@ -30,44 +30,43 @@ export default function Product() {
   return (
     <div>
       {console.log(loaiSanPham)}
-      <Content>
-        <Image
-          width={400}
-          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-        />
-        <div>
+      {console.log(cuaHang)}
+      <div className="row">
+        <div className="column">
+          <Image
+            width={400}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+        </div>
+        <div className="column">
           <h1>{product.tenSanPham}</h1>
           <h2>{product.donGia} đ</h2>
           <p>Còn {product.tonKho} sản phẩm</p>
-          <span>
-            <h3>Số lượng</h3>
-          </span>
-
+          <h3>Số lượng</h3>
           <InputNumber min={1} defaultValue={1} onChange={onChange} />
+
           <div>
             <Button type="primary" danger>
               Chọn Mua
             </Button>
           </div>
         </div>
+      </div>
 
-        <div>
-          <Descriptions title="Thông tin sản phẩm" bordered column={1}>
-            <Descriptions.Item label="Đơn vị">
-              {product.donVi}
-            </Descriptions.Item>
-            <Descriptions.Item label="Ngày đăng">
-              {product.ngayDang}
-            </Descriptions.Item>
-            <Descriptions.Item label="Mô tả sản phẩm">
-              {product.moTa}
-            </Descriptions.Item>
-            <Descriptions.Item label="Loại sản phẩm">
-              {loaiSanPham.ten}
-            </Descriptions.Item>
-          </Descriptions>
-        </div>
-      </Content>
+      <div>
+        <Descriptions title="Thông tin sản phẩm" column={1} bordered>
+          <Descriptions.Item label="Đơn vị">{product.donVi}</Descriptions.Item>
+          <Descriptions.Item label="Ngày đăng">
+            {product.ngayDang}
+          </Descriptions.Item>
+          <Descriptions.Item label="Mô tả sản phẩm">
+            {product.moTa}
+          </Descriptions.Item>
+          <Descriptions.Item label="Loại sản phẩm">
+            {loaiSanPham ? loaiSanPham.ten : ''}
+          </Descriptions.Item>
+        </Descriptions>
+      </div>
     </div>
   );
 }
