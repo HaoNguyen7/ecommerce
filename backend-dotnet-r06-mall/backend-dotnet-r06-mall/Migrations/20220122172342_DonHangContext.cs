a508034d0ed0a4b9f3c7319d6e2280e31147c080
Thang Bach <thang5217113@gmail.com>
2022-01-24 19:12:00 +0700

    fix

---
+++ backend-dotnet-r06-mall/backend-dotnet-r06-mall/Migrations/20220122172342_DonHangContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend_dotnet_r06_mall.Migrations
+{
+    public partial class DonHangContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DiaChi",
+                table: "DonHang",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiaChi",
+                table: "DonHang");
+        }
+    }
+}
---
+++ backend-dotnet-r06-mall/backend-dotnet-r06-mall/Requests/DiaChiRequest.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace backend_dotnet_r06_mall.Requests
+{
+    public class DiaChiRequest
+    {
+        public string address { get; set; }
+        public string city {get; set; }
+    }
+}
